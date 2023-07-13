7f883ec7858d27cda55fb92fca65fcf4f1b5fc0e
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-07-13 14:03:45 +0200

    add texts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import PhonePage from './page/PhonePage';
 import PhonePageDetails from './page/PhonePageDetails';
 import NavBarPage from './page/NavBarPage';
+import HomePage from './page/HomePage';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavBarPage />
 
       <Routes>
+        <Route path='/' element={<HomePage />} />
         <Route path='/phones' element={<PhonePage />}/>
         <Route path='/phones/:id' element={<PhonePageDetails />}/>
       </Routes>
---
+++ src/page/HomePage.js
@@ -0,0 +1,11 @@
+function HomePage () {
+
+  return(
+    <div className="home-page">
+      <h1>The Phone Cave</h1>
+      <h3>Feel free to see our products</h3>
+    </div>
+  )
+}
+
+export default HomePage;
